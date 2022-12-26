4d08b7f2f31df200bcb0e69e8de37d32916ca600
Kadrim <8304331+kadrim@users.noreply.github.com>
2022-12-26 18:35:13 +0100

    make download async and awaitable, cleanup

--- app/services/ytdl.service.ts
+++ app/services/ytdl.service.ts
@@ -1,18 +1,17 @@
 import * as fs from 'fs';
-import * as path from "path";
 import * as ytdl from "ytdl-core";
-import * as readline from 'readline';
+
+const { pipeline } = require('stream/promises');
 
 export class YTDLService {
     constructor() { }
 
-    public downloadWithAudio(videoURL: string, outputFile: string) {
+    public async downloadWithAudio(videoURL: string, outputFile: string) {
         const video = ytdl(videoURL, {
             quality: 'highestaudio',
         });
 
         let starttime;
-        video.pipe(fs.createWriteStream(outputFile));
         video.once('response', () => {
             starttime = Date.now();
         });
@@ -20,17 +19,11 @@ export class YTDLService {
             const percent = downloaded / total;
             const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
             const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
-            readline.cursorTo(process.stdout, 0);
-            process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-            process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
-            process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
-            process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
-            readline.moveCursor(process.stdout, 0, -1);
         });
         video.on('end', () => {
-            process.stdout.write('\n\n');
-            process.stdout.write('Done!\n');
+            console.log('finshed downloading');
         });
+        await pipeline(video, fs.createWriteStream(outputFile));
     }
 
 }
