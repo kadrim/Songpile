2b846a482ba014ae6e774777a0a3ecbc3abaf695
Kadrim <8304331+kadrim@users.noreply.github.com>
2022-12-26 18:35:32 +0100

    implement real test for ytdl

--- app/services/ytdl.service.spec.ts
+++ app/services/ytdl.service.spec.ts
@@ -1,17 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-
+import * as fs from 'fs';
+import * as path from 'path';
 import { YTDLService } from './ytdl.service';
 
 describe('YTDLService', () => {
-  let service: YTDLService;
+  let service = new YTDLService();
+  let originalTimeout;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(YTDLService);
+  beforeEach(function () {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+  });
+
+  afterEach(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    expect(service).toBeFalsy();
   });
+
+  it('should download a file', async () => {
+    let outputFile = path.resolve('./', 'test.mp4');
+    await service.downloadWithAudio('https://www.youtube.com/watch?v=aqz-KE-bpKQ', outputFile);
+    const fileStats = fs.statSync(outputFile);
+    fs.unlinkSync(outputFile);
+
+    expect(fileStats.size).toBeGreaterThan(100 * 1024 * 1024);
+  });
+
+
 });
