31c60a412376630bccbc0b26e64bf55552c43c87
David Tahir <david.tahir@tahir-it.de>
2023-01-04 09:03:29 +0100

    replace deprecated methods

--- src/app/core/services/electron/ytdl.service.spec.ts
+++ src/app/core/services/electron/ytdl.service.spec.ts
@@ -16,12 +16,12 @@ describe('YTDLService', () => {
   });
 
   it('should be created', () => {
-    const service: YTDLService = TestBed.get(YTDLService);
+    const service: YTDLService = TestBed.inject(YTDLService);
     expect(service).toBeTruthy();
   });
 
   it('should download a video-stream', async () => {
-    const service: YTDLService = TestBed.get(YTDLService);
+    const service: YTDLService = TestBed.inject(YTDLService);
     const videoData = await service.getVideoBufferWithAudio('https://www.youtube.com/watch?v=EngW7tLk6R8');
     expect(videoData.byteLength).toBeGreaterThan(800 * 1024);
   });
