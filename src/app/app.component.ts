1ffd8f36af48cef483f535faf2a6d546a1b641d4
David Tahir <david.tahir@tahir-it.de>
2023-01-06 10:13:38 +0100

    add version to title-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { APP_CONFIG } from '../environments/environment';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,8 @@ import { APP_CONFIG } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public version: string = packageJson.version;
+
   constructor(
     private electronService: ElectronService,
     private translate: TranslateService
