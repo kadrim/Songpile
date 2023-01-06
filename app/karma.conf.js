82c60126767918aa66d23755190d5c5036f7e911
Kadrim <8304331+kadrim@users.noreply.github.com>
2023-01-06 16:28:37 +0100

    add configuration for code-coverage

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -16,11 +16,11 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, '../coverage/electron'),
+      reports: [ 'html', 'lcovonly', 'text-summary', 'json-summary' ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
