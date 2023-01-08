d456fcd60e01647cb6d7f1ef6494b2226852412e
Kadrim <8304331+kadrim@users.noreply.github.com>
2023-01-08 23:21:11 +0100

    inject settings-service and adjust unit test with mocks

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../core/services/electron/settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
     console.log('SettingsComponent INIT');
-   }
+  }
 
 }
