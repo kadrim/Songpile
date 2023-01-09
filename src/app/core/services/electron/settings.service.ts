f14214a79d9df9f684805ab718c6563ab3bea66c
Kadrim <8304331+kadrim@users.noreply.github.com>
2023-01-09 16:39:48 +0100

    implement methods for settings-service

--- src/app/core/services/electron/settings.service.ts
+++ src/app/core/services/electron/settings.service.ts
@@ -11,18 +11,79 @@ export interface Settings {
   providedIn: 'root'
 })
 export class SettingsService {
-  storage: typeof ElectronJsonStorage;
-  settings: Settings;
+
+  static readonly minAudioBitrate = 96;
+  static readonly maxAudioBitrate = 320;
+  static readonly defaultAudioBitrate = 128;
+
+  private static readonly storageFilename = 'settings';
+  private static readonly storagePath = 'userData';
+  private static readonly defaultExportPath = 'music';
+
+  private storage: typeof ElectronJsonStorage;
+  private settings: Settings;
 
   constructor(
     private electronService: ElectronService
   ) {
-    this.storage = window.require('electron-json-storage');
-    this.loadSettings();
+    if (this.electronService.isElectron) {
+      this.storage = window.require('electron-json-storage');
+      this.loadSettings();
+    }
+  }
+
+  async getSettings(): Promise<Settings> {
+    return this.settings;
+  }
+
+  async setSettings(settings: Settings): Promise<Settings> {
+    this.settings = settings;
+    await this.sanitizeSettings();
+    await this.saveSettings();
+
+    return this.settings;
   }
 
   private async loadSettings() {
-    this.storage.setDataPath(await this.electronService.ipcRenderer.invoke('getPath', 'userData'));
-    const settings = await this.storage.getSync('settings');
+    this.storage.setDataPath(await this.electronService.ipcRenderer.invoke('getPath', SettingsService.storagePath));
+    this.settings = await this.storage.getSync(SettingsService.storageFilename) as Settings;
+    const dirty = await this.sanitizeSettings();
+    if(dirty) {
+      this.saveSettings();
+    }
+  }
+
+  private async saveSettings() {
+    this.storage.setDataPath(await this.electronService.ipcRenderer.invoke('getPath', SettingsService.storagePath));
+    return new Promise(resolve => {
+      this.storage.set(SettingsService.storageFilename, this.settings, (error) => {
+        if (error !== undefined) {
+          throw error;
+        }
+        resolve(null);
+      });
+    });
+  }
+
+  private async sanitizeSettings() {
+    let dirty = false;
+    if (
+      this.settings.exportPath === undefined ||
+      this.settings.exportPath === ''
+    ) {
+      this.settings.exportPath = await this.electronService.ipcRenderer.invoke('getPath', SettingsService.defaultExportPath);
+      dirty = true;
+    }
+
+    if (
+      this.settings.audioBitrate === undefined ||
+      this.settings.audioBitrate < SettingsService.minAudioBitrate ||
+      this.settings.audioBitrate > SettingsService.maxAudioBitrate
+    ) {
+      this.settings.audioBitrate = SettingsService.defaultAudioBitrate;
+      dirty = true;
+    }
+
+    return dirty;
   }
 }
